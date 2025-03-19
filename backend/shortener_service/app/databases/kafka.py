699c77787a89578c7f0d1c35e667170e8eb07b4a
VladimirKomov <v.e.komov@gmail.com>
2025-03-19 15:44:19 +0200

    Refactor Redis client and add Kafka producer support
    Introduced a `BaseAsyncClient` for shared async client logic, refactored the Redis client to use it, and added a new Kafka producer client. Updated dependencies to include `aiokafka` for Kafka support.

---
+++ backend/shortener_service/app/databases/kafka.py
@@ -0,0 +1,45 @@
+import json
+
+import aiokafka
+
+from app.core.config import config
+from app.core.logger import logger
+from app.databases.base_client import BaseAsyncClient
+
+
+class KafkaClient(BaseAsyncClient):
+    """ Kafka Producer Client """
+
+    async def _ping(self) -> bool:
+        """ Kafka does not support ping, so always return True """
+        return True
+
+    async def _create_client(self) -> None:
+        """ Create Kafka Producer """
+        async with self._lock:
+            if self.client is None:
+                try:
+                    self.client = aiokafka.AIOKafkaProducer(
+                        bootstrap_servers=config.KAFKA_URL,
+                        value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+                    )
+                    await self.client.start()
+                    logger.info("Kafka client created")
+                except Exception as e:
+                    logger.error(f"Error creating Kafka client: {e}")
+                    self.client = None
+
+    async def _close_client(self):
+        """ Close the Kafka connection """
+        if self.client:
+            try:
+                await self.client.stop()
+                logger.info("Kafka producer closed")
+            except Exception as e:
+                logger.error(f"Error closing Kafka producer: {e}")
+            finally:
+                self.client = None
+
+
+# Global Kafka client instance
+kafka_client = KafkaClient()
