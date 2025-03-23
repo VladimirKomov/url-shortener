2e753f0debef2d73318476a010150126cedea5de
VladimirKomov <v.e.komov@gmail.com>
2025-03-23 19:47:25 +0200

    Add initial implementation for URL validation service
    Introduced foundational components for the URL validation service, including validators, database clients, and Kafka integrations. Added configuration, logger, and Docker setup for MongoDB and Redis. This setup supports URL validation and messaging with robust error handling and singleton-based connections.

--- backend/shortener_service/app/services/helpers/shortener_kafka_producer_services.py
+++ backend/shortener_service/app/services/helpers/shortener_kafka_producer_services.py
@@ -3,6 +3,7 @@ from aiokafka import AIOKafkaProducer
 from app.core.config import config
 from app.core.logger import logger
 from app.databases.kafka import kafka_client
+from app.mappers.validation_kafka_mapper import ValidationKafkaMapper
 
 
 class ShortenerKafkaProducerService:
@@ -17,10 +18,10 @@ class ShortenerKafkaProducerService:
     async def send_url_validation(self, short_code: str, original_url: str) -> bool:
         """Send URL to Kafka for async validation"""
         client = await self._get_client()
-        message = {
-            "short_code": short_code,
-            "original_url": original_url,
-        }
+        message = ValidationKafkaMapper.to_kafka_message(
+            short_code=short_code,
+            original_url=original_url,
+        )
         try:
             await client.send_and_wait(config.KAFKA_TOPIC_URL_VALIDATION, message)
             logger.info(f"Sent to Kafka: {message}")
---
+++ backend/url-validator/app/core/config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    DEBUG: bool = os.getenv("DEBUG", "false").lower() in ("true", "1")
+    BASE_URL: str = os.getenv("BASE_URL")
+    REDIS_URL: str = os.getenv("REDIS_URL")
+    KAFKA_URL: str = os.getenv("KAFKA_URL")
+    KAFKA_TOPIC_URL_VALIDATION: str = os.getenv("KAFKA_TOPIC_URL_VALIDATION")
+
+config = Config()
